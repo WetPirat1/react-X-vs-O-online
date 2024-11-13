ecc041fe0c84967318a4a284148a0e736c3bbeda
WetPirat1 <EvgenSemydosky2@gmail.com>
2024-11-13 17:20:39 +0200

    added new functional count of gamers

--- components/game/game-info.jsx
+++ components/game/game-info.jsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import { Profile } from "../profile";
-import { CrossIcon } from "./icons/cross-icon";
 import { GameSymbol } from "./game-symbol";
 import { GAME_SYMBOLS } from "./constants";
 
@@ -18,60 +17,70 @@ const players = [
     symbol: GAME_SYMBOLS.CROSS,
   },
   {
-    id: 1,
-    name: "Paromovevg",
-    rating: 1230,
-    avatar: avatarSrc1,
-    symbol: GAME_SYMBOLS.CROSS,
+    id: 2,
+    name: "VereIntedinglapotur",
+    rating: 850,
+    avatar: avatarSrc2,
+    symbol: GAME_SYMBOLS.ZERO,
   },
   {
-    id: 1,
-    name: "Paromovevg",
-    rating: 1230,
-    avatar: avatarSrc1,
-    symbol: GAME_SYMBOLS.CROSS,
+    id: 3,
+    name: "Lara",
+    rating: 1400,
+    avatar: avatarSrc3,
+    symbol: GAME_SYMBOLS.TRINGLE,
   },
   {
-    id: 1,
-    name: "Paromovevg",
-    rating: 1230,
-    avatar: avatarSrc1,
-    symbol: GAME_SYMBOLS.CROSS,
+    id: 4,
+    name: "Додик",
+    rating: 760,
+    avatar: avatarSrc4,
+    symbol: GAME_SYMBOLS.SQUARE,
   },
 ];
 
-export function GameInfo({ className }) {
+export function GameInfo({ className, playersCount }) {
   return (
     <div
       className={clsx(
         className,
-        "bg-white rounded-2xl shadow-md px-8 py-4 flex justify-between"
+        "bg-white rounded-2xl shadow-md px-8 py-4 justify-between grid grid-cols-2 gap-3"
       )}
     >
-      {players.map((player) => (
-        <PlayerInfo key={player.id} playerInfo={player} />
+      {players.slice(0, playersCount).map((player, index) => (
+        <PlayerInfo
+          key={player.id}
+          playerInfo={player}
+          isRight={index % 2 === 1}
+        />
       ))}
-      <PlayerInfo />
     </div>
   );
 }
 
-function PlayerInfo({ playerInfo }) {
+function PlayerInfo({ playerInfo, isRight }) {
   return (
     <div className="flex gap-3 items-center">
-      <div className="relative">
+      <div className={clsx("relative", isRight && "order-3")}>
         <Profile
+          className="w-44"
           name={playerInfo.name}
           rating={playerInfo.rating}
           avatar={playerInfo.avatar}
-          className="w-44"
         />
         <div className="w-5 h-5 rounded-full bg-white shadow absolute -left-1 -top-1 flex items-center justify-center">
           <GameSymbol symbol={playerInfo.symbol} />
         </div>
       </div>
-      <div className="h-6 w-px bg-slate-200" />
-      <div className="text-slate-900 text-lg font-semibold">01:08</div>
+      <div className={clsx("h-6 w-px bg-slate-200", isRight && "order-2")} />
+      <div
+        className={clsx(
+          "text-slate-900 text-lg font-semibold",
+          isRight && "order-1"
+        )}
+      >
+        01:08
+      </div>
     </div>
   );
 }
--- components/profile/profile.jsx
+++ components/profile/profile.jsx
@@ -11,8 +11,8 @@ export function Profile({ className, name, rating, avatar = avatarSrc }) {
       )}
     >
       <Image src={avatar} width={48} height={48} alt="avatar" unoptimized />
-      <div>
-        <div className=" text-lg leading-tight">{name}</div>
+      <div className="overflow-hidden">
+        <div className=" text-lg leading-tight truncate ">{name}</div>
         <div className="text-slate-400 text-xs leading-tight">
           Рейтинг: {rating}
         </div>

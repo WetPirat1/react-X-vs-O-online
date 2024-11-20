a00d83bd7f0fc87bd649869af88ab38a705843a1
WetPirat1 <EvgenSemydosky2@gmail.com>
2024-11-20 23:18:45 +0200

    UI modal Window added

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,11 +6,21 @@ import {
   useGameState,
 } from "../components/game";
 import { Header } from "../components/header";
+import { GameSymbol } from "../components/game/game-symbol";
+import { UiModal } from "../components/uikit/ui-modal";
+import { UiButton } from "../components/uikit/ui-button";
 
 export default function HomePage() {
-  const [playersCount] = useState(2);
-  const { cells, currentMove, handleCellClick, nextMove, winnerSequence } =
-    useGameState(playersCount);
+  const [playersCount] = useState(4);
+  const {
+    cells,
+    currentMove,
+    handleCellClick,
+    nextMove,
+    winnerSequence,
+    handlePlayerTimeOver,
+    winnerSymbol,
+  } = useGameState(playersCount);
 
   return (
     <div className="bg-slate-50 min-h-screen">
@@ -21,7 +31,34 @@ export default function HomePage() {
           playersCount={playersCount}
           className="mt-4 "
           currentMove={currentMove}
+          isWinner={!!winnerSymbol}
+          onPlayerTimeOver={handlePlayerTimeOver}
         />
+        {winnerSymbol && (
+          <div className="my-4">
+            <GameSymbol symbol={winnerSymbol} />
+          </div>
+        )}
+        <UiModal
+          width="md"
+          isOpen={winnerSymbol}
+          onClose={() => console.log("close")}
+        >
+          <UiModal.Header>Игра завершена!</UiModal.Header>
+          <UiModal.Body>
+            <div className="text-sm">
+              Победитель: <span className="text-teal-600">Paromovevg</span>
+            </div>
+          </UiModal.Body>
+          <UiModal.Footer>
+            <UiButton size="md" variant="outline">
+              Вернуться
+            </UiButton>
+            <UiButton size="md" variant="primary">
+              Играть снова
+            </UiButton>
+          </UiModal.Footer>
+        </UiModal>
         <GameField
           className="mt-6"
           cells={cells}
@@ -29,6 +66,7 @@ export default function HomePage() {
           nextMove={nextMove}
           handleCellClick={handleCellClick}
           winnerSequence={winnerSequence}
+          winnerSymbol={winnerSymbol}
         />
       </main>
     </div>
